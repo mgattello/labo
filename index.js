5278f03056a72d10b794d97c9fb1e15b44d00c73
Marco Gattello <marco.gattello@joingsg.com>
2020-02-18 17:18:09 +0000

    Initial Test Structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Retailer, Extension, Result } =  require('./performance')
+const fs = require('fs').promises
+
+const Argos = new Retailer("Argos", "https://www.argos.co.uk")
+const Honey = new Extension("Honey")
+const HoneyResult = new Result([])
+
+async function getFinalResult (Extension, Retailer, ExtensionResult) {
+  Extension.results = await ExtensionResult.repeatTests(3, Retailer.getRetailerHomepage(), Extension.getParam(Extension.name.toLowerCase())).catch(console.log)
+  Retailer.extensionTests = await Extension
+  fs.writeFile('reports/test.json', JSON.stringify(await Retailer, null, 2))
+  return await Retailer
+}
+
+getFinalResult(Honey, Argos, HoneyResult).then(console.log).catch(console.log)
