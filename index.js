6460a633610435ab9221d6c567ce5796a6ae1f71
Marco Gattello <marco.gattello@joingsg.com>
2020-02-19 12:10:34 +0000

    Final name

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { Website, Extension, TestBuilder } =  require('./LaboPerformance')
+const { Report, Extension, TestBuilder } =  require('./LaboPerformance')
 const fs = require('fs').promises
 
-const Argos = new Website("Argos", "https://www.argos.co.uk")
+const Argos = new Report("Argos", "https://www.argos.co.uk")
 const Honey = new Extension("Honey")
 const HoneyResult = new TestBuilder()
 
-async function getFinalResult (Extension, Website, ExtensionResult) {
-  Extension.results = await ExtensionResult.repeatTests(3, Website.getRetailerHomepage(), Extension.getParam(Extension.name.toLowerCase())).catch(console.log)
-  Website.extensionTests = await Extension
-  fs.writeFile('reports/test.json', JSON.stringify(await Website, null, 2))
-  return await Website
+async function getFinalResult (Extension, Report, ExtensionResult) {
+  Extension.results = await ExtensionResult.repeatTests(3, Report.getRetailerHomepage(), Extension.getParam(Extension.name.toLowerCase())).catch(console.log)
+  Report.extensionTests = await Extension
+  fs.writeFile('reports/test.json', JSON.stringify(await Report, null, 2))
+  return await Report
 }
 
 getFinalResult(Honey, Argos, HoneyResult).then(console.log).catch(console.log)
